bf4adb7e42240a34215e3c639f2b002c47673c37
ZynexSolutions <zynexsolutions01@gmail.com>
2025-03-31 13:33:50 +0530

    commited ecommerce

--- src/app/Components/HeroBanner/HeroBanner4.jsx
+++ src/app/Components/HeroBanner/HeroBanner4.jsx
@@ -1,11 +1,19 @@
-"use client"
+"use client";
 import Slider from "react-slick";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 import Link from "next/link";
 import Image from "next/image";
 
-const HeroBanner4 = ({ subtitle, title, content, btnone, btnoneurl, btntwo, btntwourl, shape1 }) => {
-
+const HeroBanner4 = ({
+  subtitle,
+  title,
+  content,
+  btnone,
+  btnoneurl,
+  btntwo,
+  btntwourl,
+  shape1,
+}) => {
   const settings = {
     dots: false,
     infinite: true,
@@ -20,20 +28,21 @@ const HeroBanner4 = ({ subtitle, title, content, btnone, btnoneurl, btntwo, btnt
         breakpoint: 1399,
         settings: {
           slidesToShow: 3,
-        }
+        },
       },
       {
         breakpoint: 1199,
         settings: {
           slidesToShow: 2,
-        }
-      }, {
+        },
+      },
+      {
         breakpoint: 575,
         settings: {
           slidesToShow: 1,
-        }
-      }
-    ]
+        },
+      },
+    ],
   };
 
   return (
@@ -41,15 +50,25 @@ const HeroBanner4 = ({ subtitle, title, content, btnone, btnoneurl, btntwo, btnt
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-            <div className="main-heading">
+            <div className="main-heading" style={{ marginRight: "20px" }}>
               <span className="span">{subtitle}</span>
               <h1>{title}</h1>
               <p>{parse(content)}</p>
 
               <div className="space30"></div>
               <div className="buttons">
-                <Link className="theme-btn5" href={btnoneurl}>{btnone} <span><i className="bi bi-arrow-right"></i></span></Link>
-                <Link className="theme-btn6" href={btntwourl}>{btntwo} <span><i className="bi bi-arrow-right"></i></span></Link>
+                <Link className="theme-btn5" href={btnoneurl}>
+                  {btnone}{" "}
+                  <span>
+                    <i className="bi bi-arrow-right"></i>
+                  </span>
+                </Link>
+                <Link className="theme-btn6" href={btntwourl}>
+                  {btntwo}{" "}
+                  <span>
+                    <i className="bi bi-arrow-right"></i>
+                  </span>
+                </Link>
               </div>
 
               <div className="slider-area">
@@ -57,27 +76,58 @@ const HeroBanner4 = ({ subtitle, title, content, btnone, btnoneurl, btntwo, btnt
                 <div className="logo-slider owl-carousel hero_gap_4">
                   <Slider {...settings}>
                     <div className="single-slider">
-                      <Image src="/assets/custom/logo/img1.png" alt="img" width={94} height={24} />
+                      <Image
+                        src="/assets/custom/logo/img1.png"
+                        alt="img"
+                        width={94}
+                        height={24}
+                      />
                     </div>
                     <div className="single-slider">
-                      <Image src="/assets/custom/logo/img2.png" alt="img" width={94} height={24} />
+                      <Image
+                        src="/assets/custom/logo/img2.png"
+                        alt="img"
+                        width={94}
+                        height={24}
+                      />
                     </div>
                     <div className="single-slider">
-                      <Image src="/assets/custom/logo/img3.png" alt="img" width={94} height={24} />
+                      <Image
+                        src="/assets/custom/logo/img3.png"
+                        alt="img"
+                        width={94}
+                        height={24}
+                      />
                     </div>
                     <div className="single-slider">
-                      <Image src="/assets/custom/logo/img4.png" alt="img" width={94} height={18} />
+                      <Image
+                        src="/assets/custom/logo/img4.png"
+                        alt="img"
+                        width={94}
+                        height={18}
+                      />
                     </div>
                   </Slider>
                 </div>
               </div>
-              <Image className="image-shape shape-animaiton3" src={shape1} alt="img" width={332} height={162} />
+              {/* <Image
+                className="image-shape shape-animaiton3"
+                src={shape1}
+                alt="img"
+                width={332}
+                height={162}
+              /> */}
             </div>
           </div>
 
           <div className="col-lg-6">
             <div className="main-image">
-              <Image src="/assets/img/hero/hero4-img.png" alt="img" width={694} height={542} />
+              <Image
+                src="/images/ecomm.png"
+                alt="img"
+                width={694}
+                height={542}
+              />
             </div>
           </div>
         </div>
--- src/app/ecommerce/page.jsx
+++ src/app/ecommerce/page.jsx
@@ -1,54 +1,54 @@
-import React from 'react';
-import HeroBanner4 from '../Components/HeroBanner/HeroBanner4';
-import About4 from '../Components/About/About4';
-import Services4 from '../Components/Services/Services4';
-import HowWork3 from '../Components/HowWork/HowWork3';
-import Project3 from '../Components/Project/Project3';
-import Pricing1 from '../Components/Pricing/Pricing1';
-import Testimonial3 from '../Components/Testimonial/Testimonial3';
-import Blog4 from '../Components/Blog/Blog4';
-import Cta3 from '../Components/Cta/Cta3';
+import React from "react";
+import HeroBanner4 from "../Components/HeroBanner/HeroBanner4";
+import About4 from "../Components/About/About4";
+import Services4 from "../Components/Services/Services4";
+import HowWork3 from "../Components/HowWork/HowWork3";
+import Project3 from "../Components/Project/Project3";
+import Pricing1 from "../Components/Pricing/Pricing1";
+import Testimonial3 from "../Components/Testimonial/Testimonial3";
+import Blog4 from "../Components/Blog/Blog4";
+import Cta3 from "../Components/Cta/Cta3";
 
 const page = () => {
-    return (
-        <div>
-            <HeroBanner4
-                subtitle="For Ambitious Store Owners"
-                title="Reclaim Full Control Of Your Online Store"
-                content="Reimagine your online store with infinite customization. Gain total control over your data, limitless design flexibility and endless features that standard e-commerce platforms just can't match."
-                btnone="Contact Us"
-                btnoneurl="/contact"
-                btntwo="Know More"
-                btntwourl="/service"
-                shape1="/assets/img/shapes/hero4-image-shape.png"
-            ></HeroBanner4>
-            <About4
-                image1="/assets/img/about/about4-img1.png"
-                image2="/assets/img/about/about4-img2.png"
-                image3="/assets/img/about/about4-img3.png"
-                shape1="/assets/img/shapes/about3-shape1.png"
-                subTitle="About Us 👋"
-                Title="Your Partners in Building Your Next Dream Store"
-                content="Go beyond templates with Zynex Solutions. We craft high-performance, truly custom e-commerce stores. Achieve unparalleled design freedom, integrate the exact features you need, and own your platform for scalable growth and exceptional performance."
-                expNum="4+"
-                expCon="Years Experience"
-                featurelist={[
-                    "True Design Freedom",
-                    "Unparalleled Features",
-                    "Lower Maintenance Costs",
-                ]}
-                btnName="Contact Us"
-                btnUrl="/about"
-            ></About4>
-            <Services4></Services4>
-            {/* <HowWork3></HowWork3> */}
-            <Project3></Project3>
-            <Testimonial3></Testimonial3>
-            <Pricing1></Pricing1>
-            <Blog4></Blog4>
-            <Cta3></Cta3>
-        </div>
-    );
+  return (
+    <div>
+      <HeroBanner4
+        subtitle="For Ambitious Store Owners"
+        title="Reclaim Full Control Of Your Online Store"
+        content="Reimagine your online store with infinite customization. Gain total control over your data, limitless design flexibility and endless features that standard e-commerce platforms just can't match."
+        btnone="Contact Us"
+        btnoneurl="/contact"
+        btntwo="Know More"
+        btntwourl="/service"
+        shape1="/assets/img/shapes/hero4-image-shape.png"
+      ></HeroBanner4>
+      <About4
+        image1="/images/ecomm2.png"
+        image2="/images/ecomm3.png"
+        image3="/images/ecomm4.png"
+        shape1="/images/ecommshape.png"
+        subTitle="About Us 👋"
+        Title="Your Partners in Building Your Next Dream Store"
+        content="Go beyond templates with Zynex Solutions. We craft high-performance, truly custom e-commerce stores. Achieve unparalleled design freedom, integrate the exact features you need, and own your platform for scalable growth and exceptional performance."
+        expNum="4+"
+        expCon="Years Experience"
+        featurelist={[
+          "True Design Freedom",
+          "Unparalleled Features",
+          "Lower Maintenance Costs",
+        ]}
+        btnName="Contact Us"
+        btnUrl="/about"
+      ></About4>
+      <Services4></Services4>
+      {/* <HowWork3></HowWork3> */}
+      <Project3></Project3>
+      {/* <Testimonial3></Testimonial3> */}
+      <Pricing1></Pricing1>
+      <Blog4></Blog4>
+      <Cta3></Cta3>
+    </div>
+  );
 };
 
 export default page;
